f610e7948a3a6bf6d24161c7e085f651c74b7ee5
snavdeepsingh <snavdeepsingh@gmail.com>
2018-11-02 23:39:07 -0500

    ran npm build

---
+++ build/precache-manifest.c48de069adcaec9ad3c15a6b30df6470.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5a56443e51744fbde2c6a8c16762dd09",
+    "url": "/static/media/game.5a56443e.jpeg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "c2cd8e555c3e0aeff73a",
+    "url": "/static/js/main.c2cd8e55.chunk.js"
+  },
+  {
+    "revision": "499bf8bcb9159cbc401f",
+    "url": "/static/js/1.499bf8bc.chunk.js"
+  },
+  {
+    "revision": "c2cd8e555c3e0aeff73a",
+    "url": "/static/css/main.181d92c1.chunk.css"
+  },
+  {
+    "revision": "daf35ae9a5f68095b547cb4e41d752fb",
+    "url": "/index.html"
+  }
+];
